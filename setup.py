45b686bf00b0c7d3caf08f6216c9cdf59e37a95f
williamfzc <178894043@qq.com>
2018-12-24 11:26:26 +0800

    release: 0.1.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='findit',
+    version='0.1.0',
+    description='find target icon on your picture, and get its position',
+    author='williamfzc',
+    author_email='fengzc@vip.qq.com',
+    url='https://github.com/williamfzc/findit',
+    py_modules=['findit'],
+)
