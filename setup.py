7a64b8656495db104105fdc7e7234d5e983e40d2
williamfzc <178894043@qq.com>
2019-03-14 14:37:48 +0800

    release: 0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='findit',
-    version='0.2.1',
+    version='0.2.2',
     description='find target icon on your picture, and get its position',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
