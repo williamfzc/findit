7344aca9f5094d0bdb58eebc4d96fccf20623603
williamfzc <178894043@qq.com>
2019-05-09 16:28:19 +0800

    update: code improvement and some fix

--- findit.py
+++ findit.py
@@ -2,65 +2,77 @@ import cv2
 import os
 import imutils
 import numpy as np
+import typing
+
+
+def load_grey_from_path(pic_path: str) -> np.ndarray:
+    """ load grey picture (with cv2) from path """
+    raw_img = cv2.imread(pic_path)
+    return load_grey_from_cv2_object(raw_img)
+
+
+def load_grey_from_cv2_object(pic_object: np.ndarray) -> np.ndarray:
+    """ preparation for cv2 object (force turn it into gray) """
+    pic_object = pic_object.astype(np.uint8)
+    grey_pic = cv2.cvtColor(pic_object, cv2.COLOR_BGR2GRAY)
+    return grey_pic
+
+
+def fix_location(pic_object: np.ndarray, location: typing.Sequence):
+    """ location from cv2 should be left-top location, and need to fix it and make it central """
+    size_x, size_y = pic_object.shape
+    old_x, old_y = location
+    return old_x + size_x / 2, old_y + size_y / 2
 
 
 class FindItConfig(object):
     cv_method = cv2.TM_CCORR_NORMED
 
 
-def load_from_path(pic_path):
-    """ load grey picture (with cv2) from path """
-    raw_img = cv2.imread(pic_path)
-    return prepare_pic(raw_img)
-
-
-def prepare_pic(pic_object):
-    raw_img = pic_object.astype(np.uint8)
-    grey_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
-    return grey_img
-
-
 class FindIt(object):
     def __init__(self):
         # config
-        self.config = FindItConfig()
+        self.config: FindItConfig = FindItConfig()
+
         # template pic dict,
         # { pic_name: pic_cv_object }
-        self.template = dict()
+        self.template: typing.Dict[str, np.ndarray] = dict()
 
-    def load_template(self, pic_path=None, pic_object=None):
+    def load_template(self, pic_path: str = None, pic_object_list: typing.Sequence = None):
         """ load template picture """
-        assert (pic_path is not None) or (pic_object is not None), 'need path or cv object'
-        if pic_object is not None:
-            # pic_object: ('pic_name', cv_object)
-            pic_name = pic_object[0]
-            pic_data = pic_object[1]
-            self.template[pic_name] = prepare_pic(pic_data)
-            return
-        abs_path = os.path.abspath(pic_path)
-        self.template[abs_path] = load_from_path(abs_path)
+        assert (pic_path is not None) or (pic_object_list is not None), 'need path or cv object'
 
-    def find(self, target_pic_path=None, target_cv_object=None, scale=None):
+        if pic_object_list is not None:
+            # pic_object: ('pic_name', cv_object)
+            pic_name: str = pic_object_list[0]
+            pic_object: np.ndarray = pic_object_list[1]
+            self.template[pic_name] = load_grey_from_cv2_object(pic_object)
+            return
+
+        abs_path = os.path.abspath(pic_path)
+        self.template[abs_path] = load_grey_from_path(abs_path)
+
+    def find(self, target_pic_path: str = None, target_pic_object: np.ndarray = None, scale: typing.Sequence = None):
         """ start matching """
         assert self.template, 'template is empty'
         assert self.config.cv_method not in (cv2.TM_SQDIFF_NORMED, cv2.TM_SQDIFF), \
             'TM_SQDIFF & TM_SQDIFF_NORMED not supported'
-        assert (target_pic_path is not None) or (target_cv_object is not None), 'need path or cv object'
+        assert (target_pic_path is not None) or (target_pic_object is not None), 'need path or cv object'
 
         # load target
-        if target_cv_object is not None:
-            target_pic = prepare_pic(target_cv_object)
+        if target_pic_object is not None:
+            target_pic_object = load_grey_from_cv2_object(target_pic_object)
         else:
-            target_pic = load_from_path(target_pic_path)
+            target_pic_object = load_grey_from_path(target_pic_path)
 
-        result = list()
-        for each_template_path, each_template in self.template.items():
+        result: typing.List[dict] = list()
+        for each_template_path, each_template_object in self.template.items():
             # default scale
             if not scale:
                 scale = (1, 3, 10)
 
-            min_val, max_val, min_loc, max_loc = self.compare(target_pic, each_template, scale)
-            min_loc, max_loc = map(lambda i: self.fix_location(each_template, i), [min_loc, max_loc])
+            min_val, max_val, min_loc, max_loc = self.compare(target_pic_object, each_template_object, scale)
+            min_loc, max_loc = map(lambda i: fix_location(each_template_object, i), [min_loc, max_loc])
 
             # add to result list
             result.append({
@@ -71,51 +83,44 @@ class FindIt(object):
                 'max_loc': max_loc,
             })
 
+        # TODO managed by developer themselves?
         self.reset()
-        return self.build_result(target_pic_path, result)
 
-    @staticmethod
-    def fix_location(pic_object, location):
-        """ location from cv2 should be left-top location, and need to fix it and make it central """
-        size_x, size_y = pic_object.shape
-        old_x, old_y = location
-        return old_x + size_x / 2, old_y + size_y / 2
+        return {
+            'target_path': target_pic_path,
+            'config': self.config.__dict__,
+            'data': result,
+        }
 
-    def compare(self, pic, template_pic, scale):
+    def compare(self,
+                target_pic_object: np.ndarray,
+                template_pic_object: np.ndarray,
+                scale: typing.Sequence) -> typing.Sequence[float]:
         """
         match template between picture and template
         (https://www.pyimagesearch.com/2015/01/26/multi-scale-template-matching-using-python-opencv/)
 
-        :param pic:
-        :param template_pic:
+        :param target_pic_object:
+        :param template_pic_object:
         :param scale: default to (1, 3, 10)
-        :return:
+        :return: min_val, max_val, min_loc, max_loc
         """
-        pic_height, pic_width = pic.shape[:2]
+        pic_height, pic_width = target_pic_object.shape[:2]
         result_list = list()
 
         for each_scale in np.linspace(*scale):
             # resize template
-            resized_pic = imutils.resize(template_pic, width=int(template_pic.shape[1] * each_scale))
+            resized_pic = imutils.resize(template_pic_object, width=int(template_pic_object.shape[1] * each_scale))
 
             # if template's size is larger than raw picture, break
             if resized_pic.shape[0] > pic_height or resized_pic.shape[1] > pic_width:
                 break
 
-            res = cv2.matchTemplate(pic, resized_pic, self.config.cv_method)
+            res = cv2.matchTemplate(target_pic_object, resized_pic, self.config.cv_method)
             result_list.append(cv2.minMaxLoc(res))
 
         # return the max one
         return sorted(result_list, key=lambda i: i[1])[-1]
-
-    def build_result(self, target_path, result):
-        """ build final result dict """
-
-        return {
-            'target_path': target_path,
-            'config': self.config.__dict__,
-            'data': result,
-        }
 
     def reset(self):
         """ reset template, target and result """
--- sample/demo.py
+++ sample/demo.py
@@ -11,28 +11,35 @@ fi.config.cv_method = cv2.TM_CCOEFF_NORMED
 # load template picture
 fi.load_template(pic_path='./wechat_logo.png')
 # or, load cv2 object
-pic_obj = cv2.imread('./wechat_logo.png')
+pic_object = cv2.imread('./wechat_logo.png')
 # pic_object is a list (or tuple): (pic_name, cv_object)
-fi.load_template(pic_object=('wechat_logo1', pic_obj))
+fi.load_template(pic_object_list=('wechat_logo1', pic_object))
 
 # and find it
 # scale will resize template picture and auto-compare
 # (1, 3, 10) means looping (1.0, 1.2, 1.4, 1.6, ... 2.6, 2.8, 3.0) times
 result = fi.find('./wechat_screen.png', scale=(1, 3, 10))
 
-# result looks like:
-# {
-#     'some desc': 'xxx',
-#     'data': {
-#         'template1.png': {
-#             'position': (100, 500),
-#             'sim': 0.66,
-#         }
-#     }
-# }
-pprint.pprint(result)
+# after that, you can reset it or reuse it for another analysis.
+fi.reset()
 
-# assert (take this as a test case ..)
+# sample result
+pprint.pprint(result)
+# {'config': {'cv_method': 5},
+#  'data': [{'max_loc': (475.0, 344.0),
+#            'max_val': 0.984991729259491,
+#            'min_loc': (448.0, 392.0),
+#            'min_val': -0.6493372321128845,
+#            'path': 'F:\\findit\\sample\\wechat_logo.png'},
+#           {'max_loc': (475.0, 344.0),
+#            'max_val': 0.984991729259491,
+#            'min_loc': (448.0, 392.0),
+#            'min_val': -0.6493372321128845,
+#            'path': 'wechat_logo1'}],
+#  'target_path': './wechat_screen.png'}
+
+
+# take this as a test case in travis :)
 assert 'data' in result
 assert len(result['data']) == 2
 assert result['data'][0]['max_val'] > 0.98
