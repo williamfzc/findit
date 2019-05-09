ea2696fbde53ec655ca30094d07d5e63117f6014
williamfzc <178894043@qq.com>
2019-05-09 20:13:18 +0800

    update: sample

--- sample/demo.py
+++ sample/demo.py
@@ -5,20 +5,29 @@ import pprint
 
 # new one
 fi = FindIt()
-# change config
-fi.config.cv_method = cv2.TM_CCOEFF_NORMED
 
 # load template picture
-fi.load_template(pic_path='./wechat_logo.png')
+fi.load_template(pic_path='wechat_logo.png')
 # or, load cv2 object
-pic_object = cv2.imread('./wechat_logo.png')
+pic_object = cv2.imread('wechat_logo.png')
 # pic_object is a list (or tuple): (pic_name, cv_object)
 fi.load_template(pic_object_list=('wechat_logo1', pic_object))
 
 # and find it
-# scale will resize template picture and auto-compare
-# (1, 3, 10) means looping (1.0, 1.2, 1.4, 1.6, ... 2.6, 2.8, 3.0) times
-result = fi.find('./wechat_screen.png', scale=(1, 3, 10))
+result = fi.find(
+    # target pic
+    'wechat_screen.png',
+
+    # scale will resize template picture and auto-compare
+    # (1, 3, 10) means:
+    # step = (3 - 1) / 10 = 0.2
+    # and, template's shape will become (1.0, 1.2, 1.4, 1.6, ... 2.6, 2.8, 3.0) times
+    scale=(1, 3, 10),
+
+    # optional:
+    # mask feature, which supports template matching with transparent templates by defining a mask on the template
+    # mask_pic_path='mask.png',
+)
 
 # after that, you can reset it or reuse it for another analysis.
 fi.reset()
@@ -42,5 +51,5 @@ pprint.pprint(result)
 # take this as a test case in travis :)
 assert 'data' in result
 assert len(result['data']) == 2
-assert result['data'][0]['max_val'] > 0.98
-assert result['data'][1]['max_val'] > 0.98
+assert result['data'][0]['max_val'] > 0.99
+assert result['data'][1]['max_val'] > 0.99
