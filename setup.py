ddce14abe0e2ce349699563cb808f662854f45e5
williamfzc <178894043@qq.com>
2019-05-10 23:41:27 +0800

    release: 0.3.1 (because of a foolish mistake in setup.py)

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import setup
 
 
-with open('requirements.txt') as f:
-    install_requires_list = f.readlines()
+install_requirement_list = [
+    'opencv-python',
+    'imutils',
+    'numpy',
+    'loguru',
+]
 
 setup(
     name='findit',
-    version='0.3.0',
+    version='0.3.1',
     description='find target icon on your picture, and get its position',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
     url='https://github.com/williamfzc/findit',
     py_modules=['findit'],
     python_requires=">=3.6",
-    install_requires=install_requires_list
+    install_requires=install_requirement_list
 )
