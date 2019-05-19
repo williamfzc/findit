a97fd8d36f8e17a1ad401781463f9b0d0704541e
williamfzc <178894043@qq.com>
2019-05-19 16:41:25 +0800

    add: test case for server

---
+++ tests/test_server.py
@@ -0,0 +1,32 @@
+import pytest
+import subprocess
+import cv2
+import time
+
+from findit.client import FindItClient
+
+find_it_client = FindItClient()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def life_time():
+    server_process = subprocess.Popen('python3 -m findit.server --dir sample/pics', shell=True)
+    time.sleep(3)
+    yield
+    server_process.terminate()
+    server_process.kill()
+
+
+def test_heartbeat():
+    assert find_it_client.heartbeat()
+
+
+def test_analyse_with_path():
+    result = find_it_client.analyse_with_path('sample/pics/screen.png', 'wechat_logo.png')
+    assert 'data' in result
+
+
+def test_analyse_with_object():
+    pic_object = cv2.imread('sample/pics/screen.png')
+    result = find_it_client.analyse_with_object(pic_object, 'wechat_logo.png')
+    assert 'data' in result
