372ec0a55c941d0029359c140d7cbd8aa1c6469f
williamfzc <178894043@qq.com>
2019-05-21 20:17:13 +0800

    update: test case

--- tests/test_server.py
+++ tests/test_server.py
@@ -23,10 +23,26 @@ def test_heartbeat():
 
 def test_analyse_with_path():
     result = find_it_client.analyse_with_path('sample/pics/screen.png', 'wechat_logo.png')
-    assert 'data' in result
+    assert 'request' in result
+    assert 'response' in result
+    assert 'extras' in result['request']
+    assert 'template_name' in result['request']
+    assert 'data' in result['response']
 
 
 def test_analyse_with_object():
     pic_object = cv2.imread('sample/pics/screen.png')
     result = find_it_client.analyse_with_object(pic_object, 'wechat_logo.png')
-    assert 'data' in result
+    assert 'request' in result
+    assert 'response' in result
+    assert 'extras' in result['request']
+    assert 'template_name' in result['request']
+    assert 'data' in result['response']
+
+
+def test_analyse_with_extras():
+    result = find_it_client.analyse_with_path('sample/pics/screen.png', 'wechat_logo.png', a='123', b='456')
+
+    assert 'extras' in result['request']
+    assert 'a' in result['request']['extras']
+    assert 'b' in result['request']['extras']
