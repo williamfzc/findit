c0188fa08b194be4c9533b413705a1237420eecb
williamfzc <178894043@qq.com>
2019-06-30 17:43:10 +0800

    chore(sample): less target

--- sample/short.py
+++ sample/short.py
@@ -4,8 +4,6 @@ from findit import FindIt
 fi = FindIt()
 fi.load_template('wechat_logo', pic_path='pics/wechat_logo.png')
 fi.load_template('app_store_logo', pic_path='pics/app_store_logo.png')
-fi.load_template('music_logo', pic_path='pics/music_logo.png')
-fi.load_template('album_logo', pic_path='pics/album_logo.png')
 
 result = fi.find(
     target_pic_name='screen',
