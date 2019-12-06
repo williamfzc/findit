98f2acf1adfbc4033801b8aa415d706de0ba2605
williamfzc <178894043@qq.com>
2019-12-06 07:46:18 +0000

    chore: try to support 3.8

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@ from setuptools import setup, find_packages
 
 
 install_requirement_list = [
-    'opencv-python',
-    'imutils',
-    'numpy',
-    'loguru',
-    'opencv-contrib-python==3.4.2.17',
-    'scikit-learn',
-    'scikit-image',
-    'flask',
-    'gevent',
-    'scipy',
+    "opencv-python",
+    "imutils",
+    "numpy",
+    "loguru",
+    "opencv-contrib-python==3.4.2.17",
+    "scikit-learn",
+    "scikit-image",
+    "flask",
+    "gevent",
+    "scipy",
 ]
 
 setup(
-    name='findit',
-    version='0.5.6',
-    description='find target icon on your picture, and get its position',
-    author='williamfzc',
-    author_email='fengzc@vip.qq.com',
-    url='https://github.com/williamfzc/findit',
+    name="findit",
+    version="0.5.6",
+    description="find target icon on your picture, and get its position",
+    author="williamfzc",
+    author_email="fengzc@vip.qq.com",
+    url="https://github.com/williamfzc/findit",
     packages=find_packages(),
     python_requires=">=3.6",
-    install_requires=install_requirement_list
+    install_requires=install_requirement_list,
 )
